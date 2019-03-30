aade1cb548a56a86397a934c2eac4b222d33dcad
ResponsoMundus <responsomundus@gmail.com>
2019-03-30 15:05:52 -0400

    The Main Frame

--- src/cop5618/utility/Missile.java
+++ src/cop5618/utility/Missile.java
@@ -1,5 +1,25 @@
 package cop5618.utility;
 
 public class Missile {
-
+	
+	// coordinates
+	private int x, y;
+	
+	// move direction
+	private int direction;
+	
+	// reference to the battle field
+	private BattleField battlefield;
+	
+	// the id of the tank produce it
+	private int tankid;
+	
+	// move towards the direction
+	// take action if it hit any thing
+	public void move() {
+		
+		
+		
+	}
+	
 }
