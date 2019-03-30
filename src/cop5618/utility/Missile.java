b30adfb8c01a489eff0ec670238a86ce76948999
XieJunran <48813373+XieJunran@users.noreply.github.com>
2019-03-30 19:01:08 -0400

    Add files via upload

--- src/cop5618/utility/Missile.java
+++ src/cop5618/utility/Missile.java
@@ -1,25 +1,42 @@
 package cop5618.utility;
 
+import cop5618.utility.Tank.Direction;
+
 public class Missile {
 	
-	// coordinates
-	private int x, y;
+	private static int missileCounter = 0;				//Used to generate a unique missileID
 	
-	// move direction
-	private int direction;
+	public int missileID;								// A unique ID assigned
+	public int tankID;									// the id of the tank that produces it
+	public BattleField battleField;						// reference to the battle field
+	public Direction direction;							// The direction of missile
+	public int x;										// The original x coordinate the missile is generated
+	public int y;										// The original y coordinate the missile is generated
 	
-	// reference to the battle field
-	private BattleField battlefield;
+	Missile(Tank tank, BattleField battleField, Direction direction, int originalX, int originalY){
+		this.missileID = ++missileCounter;
+		this.tankID = tank.tankID;
+		this.battleField = battleField;
+		this.direction = tank.direction;
+		this.x = originalX;
+		this.y = originalY;
+	}
 	
-	// the id of the tank produce it
-	private int tankid;
-	
-	// move towards the direction
-	// take action if it hit any thing
 	public void move() {
-		
-		
-		
+		switch(direction) {
+		case LEFT:
+			x--;
+			break;
+		case RIGHT:
+			x++;
+			break;
+		case UP:
+			y++;
+			break;
+		case DOWN:
+			y--;
+			break;
+		}
 	}
 	
 }
--- src/cop5618/utility/Tank.java
+++ src/cop5618/utility/Tank.java
@@ -2,44 +2,47 @@ package cop5618.utility;
 
 public class Tank {
 	
-	final static int UP = 1;
-	final static int DOWN = 2;
-	final static int LEFT = 3;
-	final static int RIGHT = 4;
+	public static enum Direction {
+		LEFT, RIGHT, UP, DOWN
+	}
 	
-	// tank coordinates
-	private int x, y;
+	private static final Direction DEFAULT_DIRECTION = Direction.UP;	// Default deriction when a tank is created
+	private static int tankCounter = 0;										// Used to generate a unique tank id
 	
-	// tank current direction
-	private int direction;
+	public int tankID;													// An unique id assigned to the tank
+	public BattleField battleField;										// Reference to the battle field
+	public Direction direction;											// Current direction of the tank
+	public boolean moved;												// If the tank is moved in current frame
+	public boolean fired;												// If the tank fires in current frame
+	public boolean isAlive;												// If the tank is still alive
 	
-	// reference to the battle field
-	private BattleField battlefield;
+	Tank(BattleField battleField) {
+		this.tankID = ++tankCounter;
+		this.battleField = battleField;
+		this.direction = DEFAULT_DIRECTION;
+		this.moved = false;
+		this.fired = false;
+		this.isAlive = true;
+	}
 	
-	// id of the tank
-	private int tankid;
-	
-	private boolean isAlive;
+	Tank(BattleField battleField, Direction direction) {
+		this.tankID = ++tankCounter;
+		this.battleField = battleField;
+		this.direction = direction;
+		this.moved = false;
+		this.fired = false;
+		this.isAlive = true;
+	}
 	
 	// takes a keyboard input, and move according to it
-	// will detect is the next coordinate is movable
-	public void move(int d) {
-		
-		
-		
+	public void move(Direction d) {
+		direction = d;
+		moved = true;
 	}
 	
 	// fire a missile towards the direction
 	public void fire() {
-		
-		
-		
-	}
-	
-	// set direction the tank will move
-	public void setDirection() {
-		
-		
+		fired = true;
 	}
 
 }
