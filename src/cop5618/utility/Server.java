787442477c045b3bda2a902d6ae0ee54a324415c
Yihao Wu <wu.yihao.sjtu@gmail.com>
2019-04-11 11:56:44 -0400

    Update Server.java

--- src/cop5618/utility/Server.java
+++ src/cop5618/utility/Server.java
@@ -13,7 +13,7 @@ public class Server {
 	
 	public static void main (String[] args) throws Exception {
 		
-		System.out.println("Server is running.");
+		System.out.println("-----Server is running.-----");
 		
         ServerSocket listener = new ServerSocket(port);
         BattleField bf = null;
@@ -37,7 +37,7 @@ public class Server {
 			}
 		} finally {
 			listener.close();
-			System.out.println("Server terminate successfully!");
+			System.out.println("-----Server terminates!-----");
 		}
 	}
 
@@ -62,63 +62,63 @@ public class Server {
         	
         	try{
         		in = new DataInputStream(connection.getInputStream());
-    			handShake();
+    			if (!handShake()) return;
     			
        			while(bf.isEnded() == -1) {
         			
        				int msg_type = in.readInt();
        	      		
        	      		switch (msg_type) {
-       	      		// 
+       	      		// No move and fire
        	      		case 0: {
        	      			// DO NOTHING
        	      		}
        	      		break;
-       	      		// Move
+       	      		// Move up
        	      		case 1: {
        	      			tank.move(Direction.UP);
        	      		}
        	      		break;
-       	      		// 
+       	      		// Move down
        	      		case 2: {
        	      			tank.move(Direction.DOWN);
        	      		}
        	      		break;
-       	      		// 
+       	      		// Move left
        	      		case 3: {
        	      			tank.move(Direction.LEFT);
        	      		}
        	      		break;
-       	      		// 
+       	      		// Move right
        	      		case 4: {
        	      			tank.move(Direction.RIGHT);
        	      		}
        	      		break;
-       	      		// 
+       	      		// Move up and fire
        	      		case 5: {
        	      			tank.move(Direction.UP);
        	      			tank.fire();
        	      		}
        	      		break;
-       	      		// 
+       	      		// Move down and fire
        	      		case 6: {
        	      			tank.move(Direction.DOWN);
        	      			tank.fire();
        	      		}
        	      		break;
-       	      		// 
+       	      		// Move left and fire
        	      		case 7: {
        	      			tank.move(Direction.LEFT);
        	      			tank.fire();
        	      		}
        	      		break;
-       	      		// 
+       	      		// Move right and fire
        	      		case 8: {
        	      			tank.move(Direction.RIGHT);
        	      			tank.fire();
        	      		}
        	      		break;
-       	      		//
+       	      		// Fire without move
        	      		case 9: {
        	      			tank.fire();
        	      		}
@@ -131,22 +131,21 @@ public class Server {
         	} catch(IOException ioException) {
         		ioException.printStackTrace();
 			} finally {
-        		//Close connections
         		try {
         			in.close();
         			connection.close();
-        			System.out.println("Server Thread connecting to client [" + "] terminated");
+        			System.out.println("Server Thread connecting to client [" + connection.getInetAddress() + "] terminated");
 				} catch(IOException ioException) {
-					System.out.println("Disconnect with Client ");
+					System.out.println("Disconnect with Client [" + connection.getInetAddress() + "]");
 				}
 			}
         	
  		}
       	
       	// HandShake process
-      	private void handShake() throws IOException {
+      	private boolean handShake() throws IOException {
       		
-      		System.out.println("Receive handshake message!");
+      		System.out.println("-----Receive handshake message-----");
       		byte[] rcv_msg = new byte[10];
       		in.read(rcv_msg);
       		String handShakeHeadString = new String(rcv_msg);
@@ -158,10 +157,11 @@ public class Server {
       			Client client = new Client(host, bf);
       			tank = bf.AddTank(client);
       			client.start();
+      			return true;
       		}
       		else {
-      			System.err.println("Handshake unsuccessful!");
-      			// TODO
+      			System.err.println("-----Handshake unsuccessful-----");
+      			return false;
       		}
       	}
       	
